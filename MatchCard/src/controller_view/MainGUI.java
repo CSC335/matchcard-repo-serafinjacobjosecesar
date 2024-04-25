865dcdd7b07d67470310ed677d9d2ea5b433fc12
Cesar Quihuis-Romero <qromeroc@arizona.edu>
2024-04-25 09:42:28 -0700

    started implamenting shop.

--- MatchCard/src/controller_view/MainGUI.java
+++ MatchCard/src/controller_view/MainGUI.java
@@ -49,6 +49,7 @@ public class MainGUI extends Application {
 	private BorderPane everything;
 	//private Button playGame;
 	private Label mainMenu = new Label("Main Menu");
+	private Button shopButton = new Button("Shop");
 	private Button playgame = new Button("New Game");
 	private Button profile = new Button("Profile");
 	private Button quit = new Button("Quit");
@@ -138,7 +139,7 @@ public class MainGUI extends Application {
 		quit.setStyle(buttonStyles);
 		
 		menuPane.setSpacing(20);
-		menuPane.getChildren().addAll(mainMenu,playgame,profile,quit);
+		menuPane.getChildren().addAll(mainMenu,playgame,shopButton,profile,quit);
 		everything.setCenter(menuPane);
 	}
 	
@@ -279,6 +280,12 @@ public class MainGUI extends Application {
 			statsPane.getChildren().addAll(passwordChangePrompt,user,bestTimeG1,G1BestTime,bestTimeG2,G2BestTime,longestStreak,highscore,
 					profileMainMenu,newPasswordField,changePasswordButton);
 			everything.setCenter(statsPane);
+		});
+		
+		shopButton.setOnAction(event -> {
+			Account currAccount = loginPane.currentAcc;
+			Shop shopDisplay = new Shop(currAccount);
+			everything.setCenter(shopDisplay);
 		});
 		
 	}
---
+++ MatchCard/src/controller_view/Shop.java
@@ -0,0 +1,37 @@
+package controller_view;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import model.Account;
+
+public class Shop extends BorderPane{
+	private BorderPane shopPane = new BorderPane();
+	private Label cardSkins = new Label("Card Skins");
+	private Button[][] shopButtons = new Button[2][3];
+	private Account currAccount;
+	
+	public Shop(Account account) {
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 3; j++) {
+				shopButtons[i][j] = new Button();
+			}
+		}
+		currAccount = account;
+		configLayout();
+	}
+	
+	private void configLayout() {
+		HBox firstRow = new HBox();
+		HBox secondRow = new HBox();
+		VBox shopContainer = new VBox();
+		
+		firstRow.getChildren().addAll(shopButtons[0][0], shopButtons[0][1], shopButtons[0][2]);
+		secondRow.getChildren().addAll(shopButtons[1][0], shopButtons[1][1], shopButtons[1][2]);
+		shopContainer.getChildren().addAll(firstRow, secondRow);
+		shopPane.setCenter(shopContainer);
+	}
+
+}
