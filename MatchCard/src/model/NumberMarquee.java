3e124f6c98023481f55696a50ef90056b0fdf615
KeeBindz <jacob.castilloaz@gmail.com>
2024-04-30 10:39:50 -0700

    gambling addiction has been fueled

--- MatchCard/src/model/NumberMarquee.java
+++ MatchCard/src/model/NumberMarquee.java
@@ -10,6 +10,9 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.util.Random;
+
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.util.Duration;
@@ -82,11 +85,12 @@ public class NumberMarquee extends Application {
         }
     	
 
-    	
+    	//subtract bet amount from account points
+        
         final int startSpeed = 100;
-        final int endSpeed = 350;
-        final int increments = 10;
-
+        Random random = new Random();
+        final int endSpeed = random.nextInt(30) + 330;
+        int increments = random.nextInt(5) + 5;
         Timeline timeline = new Timeline();
         int totalDuration = 0;
 
@@ -104,21 +108,27 @@ public class NumberMarquee extends Application {
     private void finalizeSelection() {
     	 int finalIndex = (currentIndex - 1 + options.length) % options.length;
     	 Label finalOption = options[finalIndex];
+		 int bet = Integer.parseInt(betField.getText());
     	 if (finalOption.getText() == "Money Lost") {
     		 bottomLabel.setText("Sorry! Better luck next time.");
     	 }
     	 else if (finalOption.getText() == "Double Money") {
-    		 int bet = Integer.parseInt(betField.getText());
     		 bottomLabel.setText("You win! " + (bet*2) + " coins have been added to your account.");
     		 int i = 0;
-    		 while (i < bet) {
+    		 while (i < bet*2) {
     			 curAcc.updateCurrScore();
+    			 i++;
     		 }
-    		 betField.clear();
     	 }
     	 else if (finalOption.getText() == "Money Back") {
     		 bottomLabel.setText("Spin again!");
+    		 int i = 0;
+    		 while (i < bet) {
+    			 curAcc.updateCurrScore();
+    			 i++;
+    		 }
     	 }
+		 betField.clear();
     }
 
 	private void highlightNext() {
